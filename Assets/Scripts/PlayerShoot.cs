cd64cd399e401cbd83c18c737156a8954016eb1a
Luke <xtrememc123@gmail.com>
2025-12-03 17:21:10 -0800

    Implemented basic laser visual

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,11 +6,23 @@ public class PlayerShoot : MonoBehaviour
 {
     public float range = 100f; //Shooting Range
     public Camera fpsCam;      //Reference to our camera
+    public GameObject gun;
+    LineRenderer laserRender;
+    public float shootCooldown;
+    public float laserRenderTime;
+    bool readyShoot;
+
+    void Awake()
+    {
+        laserRender = GetComponent<LineRenderer>();
+        laserRender.enabled = false;
+        readyShoot = true;
+    }    
 
     void Update()
     {
         //Fire1 is left-click by default in Unity
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && readyShoot)
         {
             Shoot();
         }
@@ -18,12 +30,19 @@ public class PlayerShoot : MonoBehaviour
 
     void Shoot()
     {
+        readyShoot = false;
+
         RaycastHit hit; //store info about what we hit
+
+        Vector3[] laserPoints = new Vector3[2];
+        laserPoints[0] = gun.transform.position + gun.transform.forward * 0.4f;
 
         //Shooting a ray from the camera's position forward in a straight line
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log("I hit: " + hit.transform.name);
+
+            laserPoints[1] = hit.point;
 
             //Checking if the object we hit has the Target script
             Target target = hit.transform.GetComponent<Target>();
@@ -34,5 +53,27 @@ public class PlayerShoot : MonoBehaviour
                 target.TakeDamage(hit);
             }
         }
+        else
+        {
+            laserPoints[1] = fpsCam.transform.position + fpsCam.transform.forward * range;
+        }
+
+        laserRender.SetPositions(laserPoints);
+
+        laserRender.enabled = true;
+        Invoke(nameof(ResetShoot), shootCooldown);
+        Invoke(nameof(UndrawLaser), laserRenderTime);
+
+    }
+
+
+    void UndrawLaser()
+    {
+        laserRender.enabled = false;
+    }
+
+    void ResetShoot()
+    {
+        readyShoot = true;
     }
 }
